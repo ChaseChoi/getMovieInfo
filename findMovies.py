d279d9b15a17e6758e41281ce81634ac7f848ebd
ChaseChoi <caiguihao@gmail.com>
2017-10-19 21:14:04 +0800

    initial commit

---
+++ findMovies.py
@@ -0,0 +1,79 @@
+#! /usr/bin/env python3
+import requests, bs4, sys, re
+#get download link
+def getDownloadLink(movieList):
+	count = 1
+	for link in movieList:
+		detailLink = baseUrl+link['href']
+		moviePage = requests.get(detailLink)
+		moviePage.encoding='gb2312'
+		moviePage.raise_for_status()
+
+		movieSoup = bs4.BeautifulSoup(moviePage.text, "lxml")
+		downloadLinkEle = movieSoup.select('td[bgcolor="#fdfddf"]')
+		movieName = link.text
+		if len(downloadLinkEle) != 0:
+			extractedURL = downloadLinkEle[0].text
+			# fileToWrite.write("{}.name: {}\n{}".format(count, movieName, extractedURL))
+			print("{}.name: {}".format(count, movieName))
+			print(extractedURL)
+			count += 1
+
+def findNextLink(soup):
+  	nextPageEles = soup.select('a[href^="list_23_]"')
+  	getTotalPages(soup)
+  	return nextPageEles[-2]['href']
+
+def getTotalPages(soup):
+	entryEle = soup.select('td[bgcolor="#F4FAE2"]')
+	entry = entryEle[0].text
+	splitRegex = re.compile(r'\s+')
+	splitText = splitRegex.split(entry)
+	return int(splitText[-2])
+	
+
+url = 'http://www.dytt8.net/html/gndy/dyzz/'
+baseUrl = 'http://www.dytt8.net/'
+nextPageLink = url
+# define the num of pages to view
+pagesToView = 2 
+total = 0
+
+numOfArgs = len(sys.argv)
+if numOfArgs == 2:
+	pagesToView = int(sys.argv[1])
+
+print("Num of pages to view: {}".format(pagesToView))
+# fileToWrite = open('/Users/chasechoi/Documents/MyTemp/movieInfo.md', 'w')
+# fileToWrite.write("Num of pages to view: {}".format(pagesToView))
+# fileToWrite.close()
+# fileToWrite = open('/Users/chasechoi/Documents/MyTemp/movieInfo.md', 'a')
+
+for i in range(pagesToView):
+	res = requests.get(nextPageLink)
+	# read the website code
+	res.encoding='gb2312'
+	res.raise_for_status() 
+	soup = bs4.BeautifulSoup(res.text, "lxml")
+	if i == 0:
+		total = getTotalPages(soup)
+		print('Total: {}'.format(total))
+	tables = soup.select('a.ulink')
+	print("Page: {}/{}".format(i+1, pagesToView))
+
+	# fileToWrite.write("Page: {}/{}".format(i+1, pagesToView))
+	
+	getDownloadLink(tables)
+	nextPart = findNextLink(soup)
+	nextPageLink = "{}{}".format(url, nextPart)
+
+# fileToWrite.close()
+
+
+
+
+
+
+
+
+
